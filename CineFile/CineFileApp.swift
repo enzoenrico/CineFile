1429321e7d5c8388153cbbd4b7d4d6a74cfb8651
enzo <e.enrico2005@gmail.com>
2025-05-29 14:58:39 -0300

    Initial Commit

---
+++ CineFile/CineFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFileApp.swift
+//  CineFile
+//
+//  Created by Enzo Enrico on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CineFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
